3c2729b3683056fde5add12eb94ca531d1dc8539
AkitoTsukahara <a-tsukahara@macloud.jp>
2022-07-07 08:00:52 +0900

    add Controller

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts',\App\Http\Controllers\Post\IndexController::class);
+
+Route::get('/tweets', \App\Http\Controllers\Tweet\IndexController::class);
